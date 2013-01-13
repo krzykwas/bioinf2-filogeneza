6ab3cc2a75e3795565520122e358277cc8b5c65a
Krzysztof Kwaśniewski <krzykwas@gmail.com>
2013-01-13 13:38:56 +0100

    Wczytanie macierzy.

--- BioInfFilogeneza2013/src/main/java/pg/eti/kams/bioinffilogeneza2013/Main.java
+++ BioInfFilogeneza2013/src/main/java/pg/eti/kams/bioinffilogeneza2013/Main.java
@@ -1,13 +1,25 @@
 package pg.eti.kams.bioinffilogeneza2013;
 
+import java.util.Scanner;
+import org.jblas.DoubleMatrix;
+
 /**
  * Hello world!
  *
  */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        DoubleMatrix d = new DoubleMatrix(n, n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                d.put(j * n + i, s.nextDouble());
+            }
+        }
+
+        System.out.println(d);
     }
 }
